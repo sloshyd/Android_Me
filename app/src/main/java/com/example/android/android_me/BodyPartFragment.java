9cbb25f6461346d0c552e7447d5d06e23cbf78e0
sloshyd <sloshydolphin@gmail.com>
2017-09-02 08:52:07 +0100

    Completed Tasks - finished and code annotated

---
+++ app/src/main/java/com/example/android/android_me/BodyPartFragment.java
@@ -0,0 +1,38 @@
+package com.example.android.android_me;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.example.android.android_me.data.AndroidImageAssets;
+
+
+/**
+ * Created by Darren on 02/09/2017.
+ */
+
+public class BodyPartFragment extends Fragment {
+
+    public BodyPartFragment(){
+
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //View is created and returned to the activity when called by the fragmentManager
+
+        View rootView = inflater.inflate(R.layout.fragment_body_part, container, false);
+        ImageView imageView = (ImageView) rootView.findViewById(R.id.body_part_image_view);
+        //set initial view to first head in the headArray
+        imageView.setImageResource(AndroidImageAssets.getHeads().get(0));
+
+        return rootView;
+    }
+}

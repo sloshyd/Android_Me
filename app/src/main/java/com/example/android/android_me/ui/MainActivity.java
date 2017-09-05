1a63651277693d6fd5c528b85116acfd201a98b0
sloshyd <sloshydolphin@gmail.com>
2017-09-05 12:58:35 +0100

    Added Next button to fragment_master_list.xml Added Onclicklistener to Next button Added code to calculate from the postion what the body part was (head, body leg) and the correct index position within that subset. Added Intent and passed data to AndroidMeActivity

--- app/src/main/java/com/example/android/android_me/ui/MainActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -16,8 +16,11 @@
 
 package com.example.android.android_me.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.android.android_me.R;
@@ -25,7 +28,9 @@ import com.example.android.android_me.R;
 // This activity is responsible for displaying the master list of all images
 // Implement the MasterListFragment callback, OnImageClickListener
 public class MainActivity extends AppCompatActivity implements MasterListFragment.OnImageClickListener{
-
+    private int headIndex;
+    private int bodyIndex;
+    private int legsIndex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,11 +44,42 @@ public class MainActivity extends AppCompatActivity implements MasterListFragmen
         // Create a Toast that displays the position that was clicked
         Toast.makeText(this, "Position clicked = " + position, Toast.LENGTH_SHORT).show();
 
-        // TODO (2) Based on where a user has clicked, store the selected list index for the head, body, and leg BodyPartFragments
+        int typeOfBodyPart = position/12; //as int will only store 0 1 2 (as we have 35 in list)
+        int indexPosition = position - 12*typeOfBodyPart; // this calcuated the correct index position within the body type
 
-        // TODO (3) Put this information in a Bundle and attach it to an Intent that will launch an AndroidMeActivity
+        //assign the correct index to the correct body part
+        switch (typeOfBodyPart){
+            case 0:
+                headIndex = indexPosition;
+                break;
+            case 1:
+                bodyIndex = indexPosition;
+                break;
+            case 2:
+                legsIndex = indexPosition;
+                break;
+            default:
+                break;
+        }
 
-        // TODO (4) Get a reference to the "Next" button and launch the intent when this button is clicked
+        Bundle data = new Bundle();
+        data.putInt("headPart", headIndex);
+        data.putInt("bodyPart", bodyIndex);
+        data.putInt("legPart", legsIndex);
+
+        final Intent intent = new Intent(this, AndroidMeActivity.class);
+        intent.putExtras(data);
+        // Complete (2) Based on where a user has clicked, store the selected list index for the head, body, and leg BodyPartFragments
+        Button btn = (Button) findViewById(R.id.btn_next);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
+        // Complete(3) Put this information in a Bundle and attach it to an Intent that will launch an AndroidMeActivity
+
+        // Complete (4) Get a reference to the "Next" button and launch the intent when this button is clicked
 
     }
 
